485e3f25e546e4ecac35755bd0306356de2ab5bd
Luis Pomare <luispomare92@gmail.com>
2023-09-05 18:38:11 -0500

    Create sumOfNaturalNumber first version with brute force recursion

--- Recursion/sumOfNaturalNumbers.js
+++ Recursion/sumOfNaturalNumbers.js
@@ -1,11 +1,17 @@
 function sumOfNaturalNumbers(number) {
   let counter = 0;
   let result = 0;
+
   function recursionHelper() {
-    if (counter === number) return
+    if (counter === number) return;
     counter += 1;
     result += counter;
+    recursionHelper();
   }
+
+  recursionHelper();
+
+  return result;
 }
 
 export default sumOfNaturalNumbers;
