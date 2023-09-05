ec3bdd6c46d0d79fbe02694a85891229f15ff07d
Luis Pomare <luispomare92@gmail.com>
2023-09-05 18:32:30 -0500

    Add readme requirement for the 'sum of all natural numbers' function

---
+++ Recursion/sumOfNaturalNumbers.js
@@ -0,0 +1,11 @@
+function sumOfNaturalNumbers(number) {
+  let counter = 0;
+  let result = 0;
+  function recursionHelper() {
+    if (counter === number) return
+    counter += 1;
+    result += counter;
+  }
+}
+
+export default sumOfNaturalNumbers;
