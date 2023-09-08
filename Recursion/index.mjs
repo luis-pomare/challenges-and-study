14fc4735cdb6fb2dbdf3e618526cdc98507263c6
Luis Pomare <luispomare92@gmail.com>
2023-09-08 17:57:29 -0500

    Fix import statement

--- Recursion/index.mjs
+++ Recursion/index.mjs
@@ -1,5 +1,5 @@
 import counterFromCero from "./counterFromCero.mjs";
 import sumOfNaturalNumbers from "./sumOfNaturalNumbers.mjs";
 
-counterFromCero(5);
-console.log(sumOfNaturalNumbers(10));
+counterFromCero(1);
+console.log(sumOfNaturalNumbers(1));
