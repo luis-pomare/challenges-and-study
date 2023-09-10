28a448387d9de5748b0514df1b1865854fcdf2d1
Luis Pomare <luispomare92@gmail.com>
2023-09-10 13:59:17 -0500

    Parse response with json() method

--- HTTP Request/getCarsFromRoadTripWheels.js
+++ HTTP Request/getCarsFromRoadTripWheels.js
@@ -1,6 +1,7 @@
 async function getCarsFromRoadTripWheels() {
   const url = 'http://localhost:3000/api/v1/cars';
-  const response = await fetch(url);
+  let response = await fetch(url);
+  response = await response.json();
   console.log(response);
 }
 
