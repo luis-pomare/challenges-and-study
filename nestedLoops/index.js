2ce6cbf949c70dcffa250e7e88fe35e4ed0eec27
Luis Pomare <luispomare92@gmail.com>
2023-09-13 16:59:19 -0500

    Create nested loops index.js

---
+++ nestedLoops/index.js
@@ -0,0 +1,21 @@
+const information = [];
+
+information.push([
+  'Team: America',
+  'Played games: 3',
+  'goals: 3'
+]);
+
+information.push([
+  'Team: Cali',
+  'Played games: 10',
+  'goals: 0'
+]);
+
+information.push([
+  'Team: Envigado',
+  'Played games: 2',
+  'goals: 1'
+]);
+
+console.log(information);
