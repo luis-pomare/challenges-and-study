c58039711bc199ccdbdf892fd3db4863dbac688e
Luis Pomare <luispomare92@gmail.com>
2023-09-15 18:28:48 -0500

    Add suitable test cases

--- nestedLoops/meetingPlanner/index.js
+++ nestedLoops/meetingPlanner/index.js
@@ -2,7 +2,13 @@ function meetingPlanner(slots1, slots2, duration) {
 
 }
 
-const availability1 = [[10, 50], [70, 120]];
-const availability2 = [[20, 50], [70, 120]];
-const meetingDuration = 40;
+// Test cases
+const slotsA = [[10, 50], [60, 120], [140, 210]];
+const slotsB = [[0, 15], [60, 70]];
+const dur = 8;
+console.log(meetingPlanner(slotsA, slotsB, dur));  // Output: [60, 68]
 
+const slotsA2 = [[10, 50], [60, 120], [140, 210]];
+const slotsB2 = [[0, 15], [60, 70]];
+const dur2 = 12;
+console.log(meetingPlanner(slotsA2, slotsB2, dur2));  // Output: []
