dc235e787a184744a4a1a59c2b437c1263b30c9a
Luis Pomare <luispomare92@gmail.com>
2023-09-17 05:07:22 -0500

    Fix test cases

--- nestedLoops/meetingPlanner/index.js
+++ nestedLoops/meetingPlanner/index.js
@@ -4,11 +4,11 @@ function meetingPlanner(slotsA, slotsB, dur) {
 
 // Test cases
 const slotsA = [[10, 50], [60, 120], [140, 210]];
-const slotsB = [[0, 15], [20, 60]];
+const slotsB = [[0, 15], [60, 70]];
 const dur = 8;
 console.log(meetingPlanner(slotsA, slotsB, dur));  // Output: [60, 68]
 
-// const slotsA2 = [[10, 50], [60, 120], [140, 210]];
-// const slotsB2 = [[0, 15], [60, 70]];
-// const dur2 = 12;
-// console.log(meetingPlanner(slotsA2, slotsB2, dur2));  // Output: []
+const slotsA2 = [[10, 50], [60, 120], [140, 210]];
+const slotsB2 = [[0, 15], [60, 70]];
+const dur2 = 12;
+console.log(meetingPlanner(slotsA2, slotsB2, dur2));  // Output: []
