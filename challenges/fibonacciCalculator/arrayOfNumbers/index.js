f8758fce60f8f3e9dcc76c1ba8ab8d16bab1f5de
Luis Pomare <luispomare92@gmail.com>
2023-09-22 19:05:52 -0500

    Add test cases

---
+++ challenges/fibonacciCalculator/arrayOfNumbers/index.js
@@ -0,0 +1,15 @@
+const fibonacciCalculator = (number) => {
+}
+
+console.log(fibonacciCalculator(1));
+console.log(fibonacciCalculator(2));
+console.log(fibonacciCalculator(3));
+console.log(fibonacciCalculator(4));
+console.log(fibonacciCalculator(5));
+console.log(fibonacciCalculator(6));
+console.log(fibonacciCalculator(7));
+console.log(fibonacciCalculator(8));
+console.log(fibonacciCalculator(90));
+
+//This recusive solution works very well in small cuantities but in bigger ones it is going to freze
+//because it has a time complexity of o(2^n)
