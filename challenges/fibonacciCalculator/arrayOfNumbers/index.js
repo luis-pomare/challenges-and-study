b089a8769f3b8f3a2f749611bcabe5231367ad01
Luis Pomare <luispomare92@gmail.com>
2023-09-24 08:52:23 -0500

    Add numberHelper to calculate fibonacci per pos

--- challenges/fibonacciCalculator/arrayOfNumbers/index.js
+++ challenges/fibonacciCalculator/arrayOfNumbers/index.js
@@ -1,17 +1,24 @@
+const numberHelper = (number, memo = {}) => {
+  if (number in memo) return memo[number]
+  if (number <= 2) return 1;
+  return memo[number] = numberHelper((number - 2), memo) + numberHelper((number - 1), memo);
+}
+
 const fibonacciCalculator = (number, result = []) => {
   switch (number) {
     case 1:
       result.unshift(1);
       return result;
-      break;
+
     case 2:
       result.unshift(1);
       return fibonacciCalculator((number - 1), result)
-      break;
+
     default:
       if (number > 0) {
-        return number
+        return numberHelper(number);
       }
+      break;
   }
 }
 
