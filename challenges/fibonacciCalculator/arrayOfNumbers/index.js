8f0c223490a5b946492742f9af133fda1ef1bd34
Luis Pomare <luispomare92@gmail.com>
2023-09-25 13:44:40 -0500

    Add for loop solution

--- challenges/fibonacciCalculator/arrayOfNumbers/index.js
+++ challenges/fibonacciCalculator/arrayOfNumbers/index.js
@@ -5,7 +5,10 @@ const numberHelper = (number, memo = {}) => {
 }
 
 const fibonacciCalculator = (number, result = []) => {
-
+  for (let i = 1; i <= number; i += 1) {
+    result.push(numberHelper(i))
+  }
+  return result
 }
 
-console.log(fibonacciCalculator(7700));
+console.log(fibonacciCalculator(70));
