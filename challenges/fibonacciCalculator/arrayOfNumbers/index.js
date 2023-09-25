9832f8523b9ffc89a56c48bb3973b720cd2c6f07
Luis Pomare <luispomare92@gmail.com>
2023-09-25 14:05:13 -0500

    Fix fibonacci array of numbers to use ES5 modules (DRY)

--- challenges/fibonacciCalculator/arrayOfNumbers/index.js
+++ challenges/fibonacciCalculator/arrayOfNumbers/index.js
@@ -1,8 +1,5 @@
-const fibonacciCalculator = (number, memo = {}) => {
-  if (number in memo) return memo[number]
-  if (number <= 2) return 1;
-  return memo[number] = fibonacciCalculator((number - 2), memo) + fibonacciCalculator((number - 1), memo);
-}
+const fibonacciCalculator = require("../fibonacciSingleResult");
+
 
 const arrayOfNumbers = (number, counter = 1, result = []) => {
   if (counter === number) {
